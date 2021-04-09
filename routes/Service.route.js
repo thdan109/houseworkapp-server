3d141f272accb3fb16b143b4c5471d111069429b
Your Name <tranhuudanpnst@gmail.com>
2021-04-09 22:59:42 +0700

    'update94'

--- routes/Service.route.js
+++ routes/Service.route.js
@@ -5,43 +5,59 @@ var Service = require('../model/service.model')
 
 //data Serviec
 
-router.get('/dataService', function(req,res){
-   Service.find({}).then(data=>{
+router.get('/dataService', async(req,res) =>{
+   await Service.find({}).then(data=>{
       res.json(data)
    })
 })
-// router.get('/dataServiceByID', function(req,res){
-//    Service.find({ _id: req.params  }).then(data=>{
-//       res.json(data)
-//    })
-// })
+router.get('/dataServiceByID/id=:id', async(req,res) =>{
+   // console.log(req.params.id)
+   const data = await Service.findOne({ _id: req.params.id  })
+   res.send(data)
+}) 
 //add service
 
-router.post('/addService', function(req,res){
-      // console.log(req.body);
+router.post('/addService', function(req,res){      
+   const a = req.body.price.map(dt =>{
+      return  dt.value
+   })
+   a.push(req.body.dataSend.value) 
+   // console.log(a);
    Service.create({
-      nameService: req.body.nameService,
-      descriptionService: req.body.descriptionService,
-      prince: req.body.prince
-   }).then(res=>{
-
+      nameService: req.body.dataSend.nameService,
+      descriptionService: req.body.dataSend.discription,
+      prince: a
+   }).then(data=>{
+      res.status(200).send({st: 'Oke'})
    }).catch(err =>{
 
    })
 })
 
-router.post('/updataService', function(req,res){
-   const postdataService = req.body;
-   const id = req.body._id;
+router.post('/updataService', async(req,res) =>{
+   // console.log(req.body);
+   const object = req.body.data
+   const prince = []
+   for (const property in object) {
+      // console.log(`${property}: ${object[property]}`);
+      if ( (property !== 'nameService') && (property !== 'description') ){
+         prince.push(object[property])
+         // console.log(`${object[property]}`);
+         
+      }
+   }
+   console.log(prince)
+   const id = req.body.id;
+   console.log(req.body.id);
    Service.findOne({ _id: id}).then(data =>{
-      const condition = id
+      const condition = {_id: id}
       const process = {
-         nameService: req.body.nameService,
-         descriptionService: req.body.descriptionService,
-         prince: req.body.prince
+         nameService: req.body.data.nameService,
+         descriptionService: req.body.data.description,
+         prince: prince
       }
       Service.updateOne( condition, process ).then(()=>{
-         
+         res.status(200).send({Update: 'Oke'})
       })
    })
 })
@@ -50,9 +66,9 @@ router.get('/delService/id=:idService',async(req,res)=>{
    // console.log(req.params);
    const idService = req.params.idService
    Service.deleteOne({ _id: idService }).then(res =>{
-      
-   }).catch(err =>{
 
+   }).catch(err =>{
+      
    })
 })
 
